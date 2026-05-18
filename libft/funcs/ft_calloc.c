e2e862bdf36abbfcadc7e7aa604242e2445bb75c
Diego Chacon <diegdarch@hotmail.com>
2026-05-18 22:23:04 +0200

    fix ft_calloc.c and norminette in ft_strncmp.c

--- libft/funcs/ft_calloc.c
+++ libft/funcs/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: dichacon <dichacon@student.42barcelona.co  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/15 08:39:57 by dichacon          #+#    #+#             */
-/*   Updated: 2026/05/15 09:39:03 by dichacon         ###   ########.fr       */
+/*   Updated: 2026/05/18 22:20:25 by dichacon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,9 +14,18 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	char	*ptr;
+	void	*ptr;
+	size_t	total;
 
-	ptr = malloc(nmemb * size);
-	ft_bzero(ptr, nmemb);
+	if (nmemb > 0 && size > (size_t)-1 / nmemb)
+		return (NULL);
+	if (nmemb == 0 || size == 0)
+		total = 1;
+	else
+		total = nmemb * size;
+	ptr = malloc(total);
+	if (!ptr)
+		return (NULL);
+	ft_bzero(ptr, total);
 	return (ptr);
 }
--- libft/funcs/ft_strncmp.c
+++ libft/funcs/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: dichacon <dichacon@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 16:46:01 by dichacon          #+#    #+#             */
-/*   Updated: 2026/05/18 16:30:26 by dichacon         ###   ########.fr       */
+/*   Updated: 2026/05/18 22:21:40 by dichacon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -15,9 +15,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
-    if (n == 0)
-        return (0);
-    i = 0;
+	if (n == 0)
+		return (0);
+	i = 0;
 	while (s1[i] == s2[i] && i < n - 1)
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
