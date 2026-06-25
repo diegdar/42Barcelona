a9c227b8ffdda434c8e6defb3d3f569009020266
diegdar <diegdarch@hotmail.com>
2026-06-25 18:37:24 +0200

    move files main and tests outside of a_entregar folder

---
+++ cursus/ring_1/get_next_line/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "get_next_line.h"
+
+int	main(int argc, char **argv)
+{
+	int	fd;
+	char	*line;
+
+	if (argc > 1)
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+		{
+			printf("ERROR: al tratar abrir el archivo %s", argv[1]);
+			return (1);
+		}
+	}
+	else
+	{
+		fd = 0;
+		printf("Leyendo desde la terminal...\n");
+	}
+
+	line = get_next_line(fd);
+	while (line)
+	{
+		printf("\nValor de line: '%s'", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+
+	if (fd > 0)
+		close(fd);
+
+	return (0);
+}
